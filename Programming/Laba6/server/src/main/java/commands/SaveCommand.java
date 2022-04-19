d4b2ade502f6aac7908a7f52516bbbaa60010f25
Netwid <me@netwid.dev>
2022-04-19 11:02:32 +0300

    Save command

--- Programming/Laba6/server/src/main/java/commands/SaveCommand.java
+++ Programming/Laba6/server/src/main/java/commands/SaveCommand.java
@@ -32,6 +32,16 @@ public class SaveCommand implements Command {
 
     @Override
     public void execute(Request request) {
+        if (!request.client.toString().substring(1, request.client.toString().indexOf(':')).equals("127.0.0.1")) {
+            Server.print(request.client, "Вы не можете сохранить файл не с сервера\n");
+            return;
+        }
+
+        if (request.args.length < 1 || !request.args[0].equals("SuperPassword")) {
+            Server.print(request.client, "Не передан мастер-пароль или введён неверный\n");
+            return;
+        }
+
         try (FileOutputStream fos = new FileOutputStream(fileName);
              BufferedOutputStream bos = new BufferedOutputStream(fos)) {
             byte[] buffer = collectionManager.getJson().getBytes();
