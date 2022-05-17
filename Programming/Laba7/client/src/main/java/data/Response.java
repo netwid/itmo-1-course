c9b4874b07cca2c1a443e6de6aef11b898a59f54
netwid <85802435+netwid@users.noreply.github.com>
2022-05-17 06:28:55 +0300

    Revert "Auth (#12)"
    This reverts commit 6e001695f29c700e2f71799312e37398e28971f2.

--- Programming/Laba7/client/src/main/java/data/Response.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-import java.io.Serializable;
-
-public class Response implements Serializable {
-    public String toPrint;
-    public Class<? extends Serializable> toInput;
-    public String command;
-}
